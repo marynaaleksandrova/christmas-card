eaefc2ccd2b8cdd2857f4d3c32683bc9fcc22d50
Maryna Aleksandrova <hello@marynaaleksandrova.info>
2012-12-24 15:13:46 -0200

    fix image preload

--- public/index.js
+++ public/index.js
@@ -1,4 +1,23 @@
+function preload(arrayOfImages) {
+  $(arrayOfImages).each(function(){
+    $('<img/>')[0].src = this;
+  });
+}
+
+preload([
+  'images/shadow.png',
+  'images/mount-center.png',
+  'images/houses.png',
+  'images/windows-light.png',
+  'images/windows-dark.png',
+  'images/rink.png',
+  'images/rink-front.png',
+  'images/tree.png',
+  'images/general-light.png',
+]);
+
 $(window).load(function() {
+
   $('#start').addClass('active');
 
   function playCard(){
